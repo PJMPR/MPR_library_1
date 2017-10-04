ea113fbfc598d8c979b30c15ce14f5c4282a9f0f
s15324 <s15324@lab1l-14.pjwstk.edu.pl>
2017-10-04 11:08:20 +0200

    Address Add Address Class

---
+++ src/main/java/library/domain/Address.java
@@ -0,0 +1,94 @@
+package library.domain;
+
+public class Address {
+	
+	private int id;
+	private String street;
+	private String city;
+	private String postCode;
+	private String country;
+	private String houseNumber;
+	private String localNumber;
+	private String phone;
+	
+	public Address() {
+		super();
+	}
+
+	public Address(int id, String street, String city, String postCode, String country, String houseNumber, String localNumber, String phone) {
+		super();
+		this.id = id;
+		this.street = street;
+		this.city = city;
+		this.postCode = postCode;
+		this.country = country;
+		this.houseNumber = houseNumber;
+		this.localNumber = localNumber;
+		this.phone = phone;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getStreet() {
+		return street;
+	}
+	
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String getPostCode() {
+		return postCode;
+	}
+	
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	public String getHouseNumber() {
+		return houseNumber;
+	}
+	
+	public void setHouseNumber(String houseNumber) {
+		this.houseNumber = houseNumber;
+	}
+	
+	public String getLocalNumber() {
+		return localNumber;
+	}
+	
+	public void setLocalNumber(String localNumber) {
+		this.localNumber = localNumber;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}		
+	
+}
