94a8be3fb03e694757cd3ba77346ac1576226bbf
s16112 <s16112@lab1l-15.pjwstk.edu.pl>
2017-10-04 10:53:24 +0200

    IS7 Add book and author classes

---
+++ src/main/java/library/domain/Book.java
@@ -0,0 +1,49 @@
+package library.domain;
+
+public class Book {
+	private int id;
+	private String title;
+	private String publisher;
+	private int year;
+	private boolean status;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getPublisher() {
+		return publisher;
+	}
+	
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	public boolean isStatus() {
+		return status;
+	}
+	
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+}
