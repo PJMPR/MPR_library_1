612cb6b62bfa38cba93cced6920011998764e8ed
s15715 <s15715@lab1l-16.pjwstk.edu.pl>
2017-10-11 10:40:36 +0200

    ISB

--- src/main/java/library/domain/checkers/UserChecker.java
+++ src/main/java/library/domain/checkers/UserChecker.java
@@ -7,5 +7,10 @@ public class UserChecker {
 	public boolean isLoginValid(User user) {
 		return user.getLogin()!=null && !user.getLogin().isEmpty();
 	}
-		
+	public boolean isPasswordValid(User user) {
+		return user.getPassword()!=null && !user.getPassword().isEmpty();
+	}
+	public boolean isEmailValid(User user) {
+		return user.getEmail()!=null && !user.getEmail().isEmpty();
+	}
 }
--- src/test/java/library/UserCheckerTest.java
+++ src/test/java/library/UserCheckerTest.java
@@ -32,4 +32,46 @@ public class UserCheckerTest {
 		
 	}
 
+
+public void test_checker_should_check_if_user_have_password() {
+	
+	UserChecker checker = new UserChecker();
+	User userWithNullPassword = new User();
+	User userWithEmptyPassword = new User();
+	User  userWithPassword = new User();
+	
+	userWithEmptyPassword.setPassword("");
+	userWithPassword.setPassword("a");
+	
+	boolean inValidResultForNull = checker.isPasswordValid(userWithNullPassword);
+	boolean inValidResultForEmptyPassword = checker.isPasswordValid(userWithEmptyPassword);
+	boolean validResult = checker.isPasswordValid(userWithPassword);
+	
+	
+	assertFalse("check user password failed", inValidResultForNull);
+	assertFalse(inValidResultForEmptyPassword);
+	assertTrue(validResult);
+	
+}
+
+public void test_checker_should_check_if_user_have_email() {
+	
+	UserChecker checker = new UserChecker();
+	User userWithNullEmail = new User();
+	User userWithEmptyEmail = new User();
+	User  userWithEmail = new User();
+	
+	userWithEmptyEmail.setEmail("");
+	userWithEmail.setEmail("a");
+	
+	boolean inValidResultForNull = checker.isEmailValid(userWithNullEmail);
+	boolean inValidResultForEmptyEmail = checker.isEmailValid(userWithEmptyEmail);
+	boolean validResult = checker.isEmailValid(userWithEmail);
+	
+	
+	assertFalse("check user password failed", inValidResultForNull);
+	assertFalse(inValidResultForEmptyEmail);
+	assertTrue(validResult);
+	
+}
 }
