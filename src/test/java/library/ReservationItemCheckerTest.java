a1e8417734868750e6b3618ec4960f836e69ba7d
s15324 <s15324@lab1l-13.pjwstk.edu.pl>
2017-10-11 10:09:54 +0200

    IS19 reservationtest

---
+++ src/test/java/library/ReservationItemCheckerTest.java
@@ -0,0 +1,39 @@
+package library;
+
+
+
+import static org.junit.Assert.*;
+import library.domain.Book;
+import library.domain.Reservation;
+import library.domain.ReservationItem;
+import library.domain.checkers.ReservationItemChecker;
+
+import org.junit.Test;
+
+public class ReservationItemCheckerTest {
+	
+	
+	@Test
+	public void test_checker_should_have_book()
+	{
+		ReservationItemChecker checker=new ReservationItemChecker();
+		ReservationItem reservationItemNullBookReservation= new ReservationItem();
+		reservationItemNullBookReservation.setBook(null);
+		reservationItemNullBookReservation.setReservation(null);
+		ReservationItem rs= new ReservationItem();
+		rs.setBook(new Book());
+		rs.setReservation(new Reservation());
+		
+		boolean invalidBook=checker.isBookNull(reservationItemNullBookReservation);
+		boolean invalidReservation=checker.isReservationNull(reservationItemNullBookReservation);
+		boolean validReservationItemBook=checker.isBookNull(rs);
+		boolean validReservationItemReservation=checker.isReservationNull(rs);
+		
+		assertTrue(invalidBook);
+		assertTrue(invalidReservation);
+		assertFalse(validReservationItemBook);
+		assertFalse(validReservationItemReservation);
+		
+		
+	}
+}
