7939fb98807949b42422139614f8a1fab344c358
s15582 <s15582@lab1l-15.pjwstk.edu.pl>
2017-10-11 10:02:21 +0200

    IS20 Add user test

---
+++ src/test/java/library/UserCheckerTest.java
@@ -0,0 +1,31 @@
+package library;
+
+
+import library.domain.User;
+import library.domain.checkers.UserChecker;
+
+public class UserCheckerTest {
+
+	@Test
+	public void test_checker_should_check_if_user_have_login() {
+		
+		UserChecker checker = new UserChecker();
+		User userWithNullLogin = new User();
+		User userWithEmptyLogin = new User();
+		User  userWithLogin = new User();
+		
+		userWithEmptyLogin.setLogin("");
+		userWithLogin.setLogin("a");
+		
+		boolean inValidResultForNull = checker.isLoginValid(userWithNullLogin);
+		boolean inValidResultForEmptyLogin = checker.isLoginValid(userWithEmptyLogin);
+		boolean validResult = checker.isLoginValid(userWithLogin);
+		
+		
+		assertFalse("check user login failed", inValidResultForNull);
+		assertFalse(inValidResultForEmptyLogin);
+		assertTrue(validResult);
+		
+	}
+
+}
