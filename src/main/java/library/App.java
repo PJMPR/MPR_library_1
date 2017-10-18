dc756ad0387bc36b44aaab3efe1c20c164c682ac
aurbanow <aurbanow.pjwstk.edu.pl>
2017-10-18 11:13:20 +0200

    IS57 select person from db

--- src/main/java/library/App.java
+++ src/main/java/library/App.java
@@ -20,60 +20,9 @@ public class App
     	PersonRepository repository = new PersonRepository();
     	repository.createTable();
     	
-    	Person jan = new Person();
-    	jan.setName("jan");
-    	jan.setSurname("Kowalski");
+    	Person jan = repository.get(1);
     	
-    	repository.add(jan);
-    	
-    	ReservationRepository reservation_repository = new ReservationRepository();
-    	reservation_repository.createTable();
-    	
-    	ReservationItemRepository reservation_item_repository = new ReservationItemRepository();
-    	reservation_item_repository.createTable();
-    	
-    	BookRepository bookRepository = new BookRepository();
-    	bookRepository.createTable();
-    	
-    	Book book = new Book();
-    	book.setTitle("Book1");
-    	book.setPublisher("Publisher1");
-    	book.setYear(2017);
-    	book.setisAvailable(true);
-    	
-    	bookRepository.add(book);
-    	
-    	AuthorRepository authorRepository = new AuthorRepository();
-    	authorRepository.createTable();
-    	
-    	AddressRepository repositoryAddress = new AddressRepository();
-    	repositoryAddress.createTable();
-    	
-    	Address address= new Address();
-    	address.setStreet("xxx ");
-    	address.setCity("yyyy");
-    	address.setPostCode("1111");
-    	address.setCountry("qqqq");
-    	address.setHouseNumber("20");
-    	address.setLocalNumber("21");
-    	address.setPhone("000 333 111");
-    	repositoryAddress.add(address);
-    	Author author = new Author();
-    	author.setFirst_name("Name");
-    	author.setLast_name("Surname");
-    	
-    	AddressRepository repositoryAdderess = new AddressRepository();
-    	repository.createTable();
-    	UserRepository repositoryUser = new UserRepository();
-    	repositoryUser.createTable();
-    	User user = new User();
-    	user.setLogin("XXX");
-    	user.setPassword("qwerty");
-    	user.setEmail("xxx@.pjatk.pl");
-    	repositoryUser.add(user);
-    	
-    	
-    	
+    	System.out.println(jan.getName() + " " + jan.getSurname()+" " +jan.getId());
     	
         System.out.println( "koniec" );
     }
