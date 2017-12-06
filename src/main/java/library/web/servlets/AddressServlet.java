1f234fe28aed10bcf04d64e68674a55918af7b0e
KubaWisniewski <kubaw1996@gmail.com>
2017-12-06 10:33:51 +0100

    User and Address servlet

--- src/main/java/library/web/servlets/AddressServlet.java
+++ src/main/java/library/web/servlets/AddressServlet.java
@@ -1,5 +1,40 @@
 package library.web.servlets;
 
-public class AddressServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import library.dao.repos.IDatabaseCatalog;
+import library.dao.repos.impl.HsqlDbCatalogFactory;
+import library.domain.Address;
+
+@WebServlet("/Address")
+public class AddressServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public AddressServlet() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		Address address= new Address();
+		address.setCity(request.getParameter("city"));
+		address.setCountry(request.getParameter("country"));
+		address.setHouseNumber(request.getParameter("housenumber"));
+		address.setLocalNumber(request.getParameter("localnumber"));
+		address.setPhone(request.getParameter("phone"));
+		address.setPostCode(request.getParameter("postcode"));
+		address.setStreet(request.getParameter("street"));
+		IDatabaseCatalog library = new HsqlDbCatalogFactory().library();
+		library.addresses().add(address);
+		
+		
+		
+	}
 
 }
--- src/main/java/library/web/servlets/UserServlet.java
+++ src/main/java/library/web/servlets/UserServlet.java
@@ -1,5 +1,40 @@
 package library.web.servlets;
 
-public class UserServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import library.dao.repos.IDatabaseCatalog;
+import library.dao.repos.impl.DatabaseCatalog;
+import library.dao.repos.impl.HsqlDbCatalogFactory;
+import library.dao.uow.IUnitOfWork;
+import library.dao.uow.UnitOfWork;
+import library.domain.User;
+
+
+@WebServlet("/User")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public UserServlet() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		User user= new User();
+		user.setLogin(request.getParameter("login"));
+		user.setEmail(request.getParameter("email"));
+		user.setPassword(request.getParameter("password"));
+		IDatabaseCatalog library = new HsqlDbCatalogFactory().library();
+		library.users().add(user);
+	}
 
 }
