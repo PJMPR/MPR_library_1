f135fe98860a71b0af7a37a75d45791b8b3f6121
Your Name <you@example.com>
2018-01-10 10:46:45 +0100

    db relations

--- src/main/java/library/domain/Book.java
+++ src/main/java/library/domain/Book.java
@@ -1,12 +1,40 @@
 package library.domain;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="book")
 public class Book implements IHaveId {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String title;
 	private String publisher;
 	private int year;
 	private boolean isAvailable;
-	
+
+	@ManyToMany
+	private List<Author> authors;
+
+	public List<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(List<Author> authors) {
+		this.authors = authors;
+	}
+
 	public int getId() {
 		return id;
 	}
