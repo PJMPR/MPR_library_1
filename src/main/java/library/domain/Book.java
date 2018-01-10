07ba516554f0bba7312877251bdabc416ef13cef
kamip123 <kamil1997676@interia.pl>
2018-01-10 11:06:06 +0100

    IS222

--- src/main/java/library/domain/Book.java
+++ src/main/java/library/domain/Book.java
@@ -1,12 +1,41 @@
 package library.domain;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Book implements IHaveId {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String title;
 	private String publisher;
 	private int year;
+	@Column(name = "is_available")
 	private boolean isAvailable;
 	
+	@ManyToMany
+	private List<Reservation> reservations;
+	
+	public List<Reservation> getReservations() {
+		return reservations;
+	}
+
+	public void setReservations(List<Reservation> reservations) {
+		this.reservations = reservations;
+	}
+
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+
 	public int getId() {
 		return id;
 	}
--- src/main/java/library/domain/Reservation.java
+++ src/main/java/library/domain/Reservation.java
@@ -1,16 +1,44 @@
 package library.domain;
 
 import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 
-
+@Entity
 public class Reservation implements IHaveId{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+	
+	@Column(name = "reservation_date")
     private Date ReservationDate;
+	
+	@Column(name = "retrival_date")
     private Date RetirvalDate;
+	
+	@Column(name = "real_date")
     private Date RealDate;
 
-    public int getId() {
+    @ManyToMany(mappedBy = "reservations")
+    private List<Book> books;
+    
+    public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+
+	public int getId() {
         return id;
     }
 
