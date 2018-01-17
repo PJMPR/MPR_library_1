433c808b5ac31be54cf364d0eea6061dd9798772
Your Name <you@example.com>
2018-01-17 11:03:37 +0100

    Rest api for authors

--- src/main/java/library/web/rest/AuthorsResources.java
+++ src/main/java/library/web/rest/AuthorsResources.java
@@ -5,7 +5,7 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.websocket.server.PathParam;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
